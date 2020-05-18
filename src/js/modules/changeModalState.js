575d97c09d1e4ee890e938446b6550a958501cdd
Petr Nikitin <nikitablvck@gmai.com>
2020-05-18 15:26:02 +0300

    calc window modal

---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,11 @@
+const changeModalState = () => {
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
+        windowWidth = document.querySelector('#width'),
+        windowHeight = document.querySelector('#height'),
+        windowType = document.querySelector('#view_type'),
+        windowProfile = document.querySelectorAll('#view_type');
+
+
+};
+
+export default changeModalState;
