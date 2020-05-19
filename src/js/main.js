7ab4d7c5638a24c42e6ca9dec1a35268ed8735ba
petrrrnikitin <nikitablvck@gmail.com>
2020-05-19 19:15:18 +0300

    calc modal and timer ready

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,21 @@ import "./slider";
 import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from "./modules/forms";
+import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     let modalState = {};
+    changeModalState(modalState);
+    let deadline = '2020-12-31';
+
 
     modals();
     tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider','.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline' )
-    forms();
+    forms(modalState);
+    timer('.container1', deadline);
 })
